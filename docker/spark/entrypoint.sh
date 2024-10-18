4bc1c01a2bafb4944db6d590c643ed7a2d5fd1fc
marcoverl <marco.verlato@pd.infn.it>
2024-10-18 19:55:50 +0200

    Update entrypoint.sh
    add unshare command to allow for fusermount in unpacked

--- docker/spark/entrypoint.sh
+++ docker/spark/entrypoint.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+unshare -c -m --keep-caps
 # echo commands to the terminal output
 set -ex
 
